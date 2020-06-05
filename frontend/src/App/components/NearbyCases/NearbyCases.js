c257edaae22b83b7f2dbe325b843c0c377e9cb92
Mateus Mendonca <mateus.mendonca@softruck.com>
2020-06-05 10:00:30 -0300

    Change map library in the where-to-test page

--- frontend/src/App/components/NearbyCases/NearbyCases.js
+++ frontend/src/App/components/NearbyCases/NearbyCases.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MapComponent from '../Map/Map'
 import './NearbyCases.scss';
 
-import Map from './Map'
+const NearbyCases = () => {
+  const [isMarkerShown, setIsMarkerShown] = useState(false)
+  const [occurrences, setOcurrences] = useState([])
+  const [error, setError] = useState(null)
 
-class NearbyCases extends React.Component {
-  render () {
-    return (
-      <div className="NearbyCases">
-        <div className="content-title">
-          <h1>Casos Próximos</h1>
-        </div>
-        <div className="content-body">
-          <Map />
-        </div>
-      </div>
-    );
+  const fetchCases = () => {
+    fetch(`http://localhost:3000/cases`, {
+      mode: 'cors',
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(data => setOcurrences(data.data))
+    .catch(err => setError(err))
   }
+
+  useEffect(() => {
+    fetchCases()
+  }, [])
+
+  const showMarker = () => {
+    setIsMarkerShown(true)
+  }
+
+  const handleMarkerClick = () => {
+    setIsMarkerShown(false)
+    showMarker()
+  }
+
+  return (
+    <div className="NearbyCases">
+      <div className="content-title">
+        <h1>Casos Próximos</h1>
+      </div>
+      <div className="content-body">
+        {
+          error ?
+          null :
+          <MapComponent
+            isMarkerShown={isMarkerShown}
+            onMarkerClick={handleMarkerClick}
+            occurrences={occurrences}
+          />
+        }
+      </div>
+    </div>
+  );
 }
 
 export default NearbyCases;
